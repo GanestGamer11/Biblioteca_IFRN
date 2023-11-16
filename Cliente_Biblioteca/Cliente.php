fcc0d12a198953d48ab6594f1f5e31803e2b6e4b
ganestgamer11 <ganestgamer11@gmail.com>
2023-11-16 08:25:45 -0300

    cadastrar e exibir livro, cliente

--- Cliente_Biblioteca/Cliente.php
+++ Cliente_Biblioteca/Cliente.php
@@ -11,6 +11,16 @@ $options = array(
     "5" => "Deletar Livro",
     "6" => "Sair"                
 );
+
+$formulario_base = array(
+    "email" => "admin@admin.com",
+    "titulo" => "",
+    "autor" => "",
+    "descricao" => "",
+    "editora" => "",
+    "genero" => "",
+);
+
 popen("cls","w");
 $logado = false;//fazer algo com isso depois
 while(true){
@@ -32,8 +42,60 @@ while(true){
             echo"\n";
             break;
         case "2":
+            popen("cls","w");
+            echo"\n-----Cadastro de Livro-----\n";
+            $form = $formulario_base;
+            echo "Digite o título do livro:\n";
+            $titulo = readline();
+            echo "Digite o autor do livro:\n";
+            $autor = readline();
+            echo "Digite a descrição do livro:\n";
+            $descricao = readline();
+            echo "Digite a editora do livro:\n";
+            $editora = readline();
+            echo "Digite o gênero do livro:\n";
+            $genero = readline();
+
+            $form['titulo'] = $titulo;
+            $form['autor'] = $autor;
+            $form['descricao'] = $descricao;
+            $form['editora'] = $editora;
+            $form['genero'] = $genero;
+
+            $resp = enviar_requisicao("$url_api/livros",
+                metodo: "POST",
+                corpo: json_encode($form),
+                cabecalhos: ['Content-type:application/json']
+            );
+
+            if($resp['codigo'] == "201"){
+                echo "--LIVRO CADASTRADO COM SUCESSO--\n";
+            }else if($resp['codigo'] == "400"){
+                echo "--USUARIO PRECISA ESTAR LOGADO--\n";
+            }
+
             break;
         case "3":
+            popen("cls","w");
+            echo"\n-----Exibir Livro-----\n";
+            echo "Digite o id do livro\n";
+            $id = intval(readline());
+            $resp = enviar_requisicao("$url_api/livros/{$id}");
+
+            if($resp['codigo'] == "20"){
+                echo "--LIVRO ENCONTRADO--\n";
+            }else if($resp['codigo'] == "502"){
+                echo "--O LIVRO NAO EXISTE--\n";
+                break;
+            }
+
+            $resp_json = json_decode($resp['corpo'], true);
+
+            echo "\n-----".$resp_json['message']."-----\n";
+            
+            foreach ($resp_json["result"] as $key => $value) {
+                echo ucfirst("$key: ").$value."\n";
+            }
             break;
         case "4":
             break;
